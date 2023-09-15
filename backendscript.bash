fcd2bf1e7d6423006f7befc42bc53de641258b60
nguyenducphucc <jonybounce@gmail.com>
2023-09-14 23:14:06 -0500

    settingEnv

--- backendscript.bash
+++ backendscript.bash
@@ -1,5 +1,7 @@
 pip3 install pipenv
 open http://localhost:8000/api/todos
+pipenv install django
 pipenv run python3 ./back_end/manage.py runserver
 
 # https://www.digitalocean.com/community/tutorials/build-a-to-do-application-using-django-and-react
+# Using python 3.x
